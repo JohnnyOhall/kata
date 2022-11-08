af96d8040065aeff8d1c7a8133d5ded5b2e34553
John O'Halloran <john.ohalloran@telus.com>
2022-11-08 16:38:23 -0500

    updating camel-case & multiplication-tables.js finished

--- stretch/camel-case-s.js
+++ stretch/camel-case-s.js
@@ -5,21 +5,23 @@ Our job is to convert these strings into camel cased strings.
 
 const camelCase = input => {
   newString = ""; //placeholder to store camelCase value
+  input = input.toLowerCase(); //change to all lowercase
 
   for (let i = 0; i < input.length; i++){
-    if (input[i] === " "){ //checks for " " at string index
+    if (input[i] === " " && input[i + 1] === " "){/*do nothing on this loop*/} 
+    else if (input[i] === " "){ //checks for " " at string index
       newString += input[i + 1].toUpperCase(); // skips space and changes next character to uppercase
       i++ // increases loop counter by 1 to prevent duplicate characters after space is removed
-    } else {
-      newString += input[i]; //adds string index character to 'newString' if " " not found
-    }
-  }
+    } 
+    else {newString += input[i];} //adds string index character to 'newString' if " " not found
+  };
 
-  return newString // returns output
+  return newString; // returns output
 };
 
 
 // TEST CASES //
 console.log(camelCase("this is a string"));
 console.log(camelCase("loopy lighthouse"));
-console.log(camelCase("supercalifragalisticexpialidocious"));
+console.log(camelCase("supercAlifragAlisticexpialidocious"));
+console.log(camelCase("a  b  c  deefg Hi"));
---
+++ stretch/multiplication-table-s.js
@@ -0,0 +1,30 @@
+/* Kata 10 - Multiplication Table
+We will be given a number as our input data. This number is the highest value of our 
+multiplication table. Our job is to generate a multiplication table for the values 
+from 1 to the provided number.
+*/
+
+const multiplicationTable = maxValue => {
+  table = ""; temp = ""; // Empty placeholders for storing loop data
+  init = 0; // To check if first line or not during inital loop
+  
+  for (let x = 1; x <= maxValue; x++) { // Loop throw X row
+    for (let y = 1; y <= maxValue;y++) { 
+      temp += (x * y) + " "; // multiple x value by y on each loop
+    }
+
+    if (init === 0){table += temp;}  //start first row without \n char
+    else {table += "\n" + temp;} //start new line for each x loop
+    
+    temp = ""; // clears temp so results are not doubled on same line
+    init++ //increases init so each loop after first include \n char
+  };
+
+  return table; // return result
+};
+
+
+// TEST CASES //
+console.log(multiplicationTable(1));
+console.log(multiplicationTable(5));
+console.log(multiplicationTable(10));
